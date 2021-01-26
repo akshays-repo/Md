390e89db839750f6a0fe778bde67c2cc466d1ffa
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-01-26 21:59:05 +0530

    added patient page

--- src/pages/Dashboard_Content/MyPatients/index.js
+++ src/pages/Dashboard_Content/MyPatients/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
-import Dashboard_Content from '..'
+import { Table, Tag, Space } from 'antd';
+import Dashboard_Content from '..';
 
-const Dashboard_MyPatients = () =>{
-    const MyPatients = () =>{
-        return(
-            <div>
-            <div className="search">
-            
-            </div>
-            </div>
-        )
-    }
-    return <Dashboard_Content content={MyPatients()} />
-}
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Phone',
+    dataIndex: 'phone',
+    key: 'phone',
+  },
+  {
+    title: 'Blood Group',
+    dataIndex: 'bloodgroup',
+    key: 'bloodgroup',
+  },
+];
 
-export default Dashboard_MyPatients
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    id: 'New York No. 1 Lake Park',
+    phone: '',
+    bloodgroup: '',
+  },
+  {
+    key: '2',
+    name: 'John Brown',
+    age: 32,
+    id: 'New York No. 1 Lake Park',
+    phone: '',
+    bloodgroup: '',
+  },
+  {
+    key: '3',
+    name: 'John Brown',
+    age: 32,
+    id: 'New York No. 1 Lake Park',
+    phone: '12486258',
+    bloodgroup: 'AB',
+  },
+];
+
+const Dashboard_MyPatients = () => {
+  const MyPatients = () => {
+    return (
+      <div>
+        <div className="search">
+          <form>
+            <input type="text" placeholder="Search by Name or ID" />
+            <button>Search</button>
+          </form>
+          <button>Add button</button>
+        </div>
+        <div className="patient_name"></div>
+        <Table columns={columns} dataSource={data} />,
+      </div>
+    );
+  };
+  return <Dashboard_Content content={MyPatients()} />;
+};
+
+export default Dashboard_MyPatients;
