d78255d4edc457d55c6e5dd997f1c0f4f377d7fb
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-01-27 20:21:59 +0530

    added edit in dashboard

--- src/pages/Dashboard_Content/Branch/branchListTable.js
+++ src/pages/Dashboard_Content/Branch/branchListTable.js
@@ -1,73 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Table, Tag, Space, Modal, Button } from 'antd';
+import EditBranch from './editBranch';
 
-import { Table, Tag, Space } from 'antd';
+const BranchListTable = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [editId , setEditId] = useState('hyy');
+  const openEditModal = (id) => {
+      setEditId(id)
+    setIsModalVisible(true);
+  };
 
-const BranchListTable = () =>{
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-          render: text => <a>{text}</a>,
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
-        {
-          title: '',
-          key: 'action',
-          render: () => (
-            <Space size="middle">
-              <button>edit</button>
-              <button>delete</button>
-            </Space>
-          ),
-        },
-      ];
-      
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          age: 32,
-          address: 'New York No. 1 Lake Park',
-          tags: ['nice', 'developer'],
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          age: 42,
-          address: 'London No. 1 Lake Park',
-          tags: ['loser'],
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          age: 32,
-          address: 'Sidney No. 1 Lake Park',
-          tags: ['cool', 'teacher'],
-        },
-      ];
-    return(
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-        <div>
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '',
+      key: 'action',
+      render: (record) => (
+        <Space size="middle">
+          <button onClick={() => openEditModal(record.key)}>edit</button>
+          <button>delete</button>
+        </Space>
+      ),
+    },
+  ];
 
-<div>
-<Space direction="horizontal">
-    <button>Filter</button>
-</Space>
-</div>
-            <div>
-            <Table columns={columns} dataSource={data} />
-            </div>
-        </div>
-    )
-}
-export default BranchListTable
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  return (
+    <div>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+       <EditBranch id={editId}/>
+      </Modal>
+      <div>
+        <Space direction="horizontal">
+          <button>Filter</button>
+        </Space>
+      </div>
+      <div>
+        <Table columns={columns} dataSource={data} />
+      </div>
+    </div>
+  );
+};
+export default BranchListTable;
---
+++ src/pages/Dashboard_Content/Branch/editBranch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const EditBranch = ({id}) =>{
+    return(
+        <div>
+           <h1>{id}</h1> 
+        </div>
+    )
+}
+export default EditBranch
