d985ffcf65586f69fa0ae7ac529f824103ebb54e
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-01-27 09:58:12 +0530

    added Forms section

--- src/pages/Dashboard_Content/Forms/index.js
+++ src/pages/Dashboard_Content/Forms/index.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import { Table, Tag, Space } from 'antd';
 import Dashboard_Content from '..'
 
 const Dashboard_Forms = () =>{
+    const columns = [
+        {
+          title: 'Patient',
+          dataIndex: 'patient',
+          key: 'patient',
+        },
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Submission',
+          dataIndex: 'submission',
+          key: 'submission',
+          render:text => <a>{'Download File'}</a>,
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          render: () => (
+            <Space size="middle">
+              <button>view</button>
+            </Space>
+          ),
+        },
+      ];
+      
+      const data = [
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ];
+      
     const Forms = () =>{
         return(
             <div>
-            Forms
+           <Table columns={columns} dataSource={data} />
             </div>
         )
     }
