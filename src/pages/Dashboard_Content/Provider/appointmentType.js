a7154352b4b55de15dd661907b5d06f155ad1ef9
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-01-28 23:09:22 +0530

    added mxin for button

--- src/pages/Dashboard_Content/Provider/appointmentType.js
+++ src/pages/Dashboard_Content/Provider/appointmentType.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Space } from 'antd';
+import { Form, Input, Button, Radio , Select} from 'antd';
+const AppointmentTypes = () => {
+  const [appointmentField , setAppointmentField] = useState([1])
+  const AddNewField = () =>{
+    setAppointmentField([...appointmentField , appointmentField.push(1)])
 
-const AppointmentTypes = () =>{
-    const displayfield = () =>{
-        return
-    }
-    return(
-        <div className="appointment-type">
-            <div className="header">
-            <h2>Appointment types</h2>
-            <button onClick={()=>{}}>
-            <i class="fas fa-plus"></i>
-            </button>
-            </div>
+  }
 
-        </div>
-    )
-}
+  return (
+    <div className="appointment-type">
+      <div className="header">
+        <h2>Appointment types</h2>
+        <button onClick={() =>AddNewField()}>
+          <i class="fas fa-plus"></i>
+        </button>
+      </div>
+      <div className="field-section">
+        {appointmentField?.map(() => (
+          <div>
+            <Form>
+              <Form.Item label="Select">
+                <Select>
+                  <Select.Option value="demo">Demo</Select.Option>
+                  <Select.Option value="demo">Demo</Select.Option>{' '}
+                  <Select.Option value="demo">Demo</Select.Option>{' '}
+                  <Select.Option value="demo">Demo</Select.Option>{' '}
+                  <Select.Option value="demo">Demo</Select.Option>{' '}
+                  <Select.Option value="demo">Demo</Select.Option>{' '}
+                  <Select.Option value="demo">Demo</Select.Option>
+                </Select>
+              </Form.Item>
+            </Form>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default AppointmentTypes;
