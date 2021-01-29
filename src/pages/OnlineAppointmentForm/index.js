670cc169e1e4d6bd19f722e446303cc2a85c2f3f
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-01-29 19:43:45 +0530

    online book bg

--- src/pages/OnlineAppointmentForm/index.js
+++ src/pages/OnlineAppointmentForm/index.js
@@ -4,111 +4,79 @@ import { PatientCreationSchema } from '_utils/Schemas';
 import { TextField } from 'formik-material-ui';
 import { message, Button, Row, Col } from 'antd';
 import { PoweroffOutlined } from '@ant-design/icons';
-import callApi from '_utils/callApi';
+
 const OnllineAppointmentForm = () => {
+  const handleFormSubmission = () => {
+    //Handle submission goes here
+  };
   const [loadings, setLoadings] = useState(false);
   const innerForm = useRef();
-  const handleFormSubmission = async values => {
-    try {
-      const result = await callApi('', {
-        method: 'POST',
-        body: JSON.stringify(values),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    } catch (err) {
-      console.log(err);
-      // message.error('Please try again');
-    }
-  };
-
   return (
-    <>
-      <Formik
-        enableReinitialize={true}
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-          phone: '',
-          address: '',
-          avatarlocation: '',
-          phone: '',
-          status: '',
-          image: '',
-          userTypeId: 3,
-          package_id: '',
-          hospital_id: '',
-        }}
-        validationSchema={PatientCreationSchema}
-        onSubmit={handleFormSubmission}
-        innerRef={innerForm}
-      >
-        {({ handleSubmit, touched, errors, isSubmitting }) => (
-            <div className="online-appointment-form">
-                <h2>Please enter your exact information</h2>
-          <Form
-            className="login__form"
-            handleSubmit={handleSubmit}
-          >
-              <p>
-                <label className="input-file mr-4">
-                  Upload New picture
-                  <input
-                    type="file"
-                    id="profileImage"
-                    onChange={file => {
-                      const result = URL.createObjectURL(file.currentTarget.files[0]);
-                    }}
-                  />
-                </label>
-              </p>
-                {' '}
-                <label>First Name</label>
-                <p>
-                  <Field component={TextField} name="firstName" placeholder="" type="text"></Field>
-                  {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
-                </p>{' '}
-                {' '}
-                <label>Last Name</label>
-                <p>
-                  <Field component={TextField} name="lastName" placeholder="" type="text"></Field>
-                  {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
-                </p>
-                <label>Email</label>
-                <p>
-                  <Field component={TextField} name="email" placeholder="" type="text"></Field>
-                  {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
-                </p>{' '}
-                {' '}
-                <label>Phone</label>
-                <p>
-                  <Field component={TextField} name="phone" placeholder="" type="phone"></Field>
-                  {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
-                </p>{' '}
-                {' '}
-                <label>Address</label>
-                <p>
-                  <Field component={TextField} name="name" placeholder="" type="text"></Field>
-                  {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
-                </p>
-
-            <Button
-              className="mt-5"
-              htmlType="submit"
-              disabled={isSubmitting}
-              loading={loadings}
-              className="submitbutton"
-            >
-              Book Appointment
-            </Button>
-          </Form>
-            </div>
-
-        )}
-      </Formik>
-    </>
+    <div className="onlinebookWrapper">
+      <div className="login-page">
+        <Formik
+          enableReinitialize={true}
+          initialValues={{}}
+          validationSchema={PatientCreationSchema}
+          onSubmit={handleFormSubmission}
+          innerRef={innerForm}
+        >
+          {({ handleSubmit, touched, errors, isSubmitting }) => (
+            <Form className="login__form" handleSubmit={handleSubmit}>
+              <h1 className="text-center">Fill the Form To Book A Appointment</h1>{' '}
+              <div className="loginInput">
+                <Field
+                  label="Full Name"
+                  component={TextField}
+                  name="name"
+                  placeholder=""
+                  type="text"
+                ></Field>
+                {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
+              </div>{' '}
+              <div className="loginInput">
+                <Field
+                  label="Email"
+                  component={TextField}
+                  name="email"
+                  placeholder=""
+                  type="text"
+                ></Field>
+                {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
+              </div>{' '}
+              <div className="loginInput">
+                <Field
+                  label="Phone Number"
+                  component={TextField}
+                  name="phone"
+                  placeholder=""
+                  type="text"
+                ></Field>
+                {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
+              </div>{' '}
+              <div className="loginInput">
+                <Field
+                  label="Address"
+                  component={TextField}
+                  name="address"
+                  placeholder=""
+                  type="text"
+                ></Field>
+                {touched.name && errors.name ? <div className="errormsg">{errors.name}</div> : ''}
+              </div>{' '}
+              <Button
+                htmlType="submit"
+                disabled={isSubmitting}
+                loading={loadings}
+                className="button-square edit-button"
+              >
+                Book A Appointment
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 export default OnllineAppointmentForm;
