336f8c3b2a0b97b4ad6c5f3d11dfa4ffa3e449c3
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-02-05 19:21:54 +0530

    removed sucess message

--- src/reducers/ComponentReducer/onlineBookingForm.js
+++ src/reducers/ComponentReducer/onlineBookingForm.js
@@ -9,7 +9,6 @@ import { message } from 'antd';
 export const OnlineBookingFormReducer = (state = CustomFormState, action) => {
   switch (action.type) {
     case 'CREATE_ONLINE_APPOINTMENT':
-      message.success('CUSTOMFORM CREATED SUCCESSFULLY');
       return { error: action.error, payload: action.payload, message: 'sucess' ,};
     default:
       return state;
