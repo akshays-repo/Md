471a13bbcfe25834d565eeff66ebb6a3ff5e28f2
saiju <saiju.j@riolabz.com>
2021-02-09 02:00:23 +0530

    85% completed

--- src/pages/LandingPage/banner.js
+++ src/pages/LandingPage/banner.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Row, Col } from 'antd';
+import doctorImg from './assets/images/doctor.png'
 
 const FossilMdBanner = () =>{
     return(
         <div>
             <div className="landing-banner">
+<div className="container">
+    <Row>
+    <Col span={10}>
+        <div className="doctorPic">
+            <img src={doctorImg}/>
+        </div>
+    </Col>
+    <Col span={14}>
+    <div className="caption">
+        <h3 style={{color: '#69b9ff'}}>Amet consectetur adipisicing elit sed do eiusmod</h3>
+        <h1>Search Doctor</h1>
+<h2 >Make an Appointment</h2>
+    </div>
+    </Col>
+    </Row>
+</div>
 
             </div>
         </div>
--- src/pages/LandingPage/steps.js
+++ src/pages/LandingPage/steps.js
@@ -1,25 +1,83 @@
-import React from 'react'
-import { Row , Col} from 'antd'
+import React from 'react';
+import { Row, Col } from 'antd';
+import icon1 from './assets/images/icon-1.png'
+import icon2 from './assets/images/icon-2.png'
+import icon3 from './assets/images/icon-3.png'
+import icon4 from './assets/images/icon-4.png'
 
-const FossilMdSteps = () =>{
-    return(
-
-            <div className="landingSteps">
-<div className="processWrapper">
-    <div className="container">
-        <Row>
-            <Col lg={24}>
-                <div className="heading text-center">
-                    <h1>Connect with Patients any Time, Anywhere
-</h1>
-                </div>
+const FossilMdSteps = () => {
+  return (
+    <div className="landingSteps">
+      <div className="processWrapper">
+        <div className="container">
+          <Row className="w100">
+            <Col span={24}>
+              <div className="heading">
+                <h1>
+                  <span className="lightFont">Connect with</span> Patients{' '}
+                  <span className="lightFont">any</span> Time, Anywhere
+                </h1>
+                <h3 style={{ color: 'rgb(5, 214, 231)' }}>Appointment Process</h3>
+              </div>
             </Col>
-        </Row>
-    </div>
-
-</div>
-
+          </Row>
+<div className="stepsAppointment mt6">
+          <Row>
+          <Col span={6}>
+            <div className="whiteBox">
+              <div className="iconBox">
+<img src={icon1} />
+              </div>
+              <div className="footerCaption">
+<h5>
+Appointment 
+Reminder
+</h5>
+              </div>
+            </div>
+          </Col>
+          <Col span={6}>
+          <div className="whiteBox">
+          <div className="iconBox">
+<img src={icon2} />
+              </div>
+          <div className="footerCaption">
+<h5>
+Chat
+</h5>
+              </div>
+              </div>
+          </Col>
+          <Col span={6}>
+          <div className="whiteBox">
+          <div className="iconBox">
+<img src={icon3} />
+              </div>
+          <div className="footerCaption">
+<h5>
+Booking 
+Reminder
+</h5>
+              </div>
+              </div>
+          </Col>
+          <Col span={6}>
+          <div className="whiteBox">
+          <div className="iconBox">
+<img src={icon4} />
+              </div>
+          <div className="footerCaption">
+<h5>
+Review Doctor
+</h5>
+              </div>
+              </div>
+          </Col>
+          </Row>
+          </div>
         </div>
-    )
-}
-export default FossilMdSteps
+      </div>
+    </div>
+  );
+};
+export default FossilMdSteps;
