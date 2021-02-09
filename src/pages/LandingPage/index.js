728505c8df71c5febdf121c7f7a9c0dfdfb1d7a9
saiju <saiju.j@riolabz.com>
2021-02-09 10:06:52 +0530

    steps hover done

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import FossilMdBanner from './banner'
-import FossilMdAbout from './about'
-import FossilMdPricing from './pricing'
-import FossilMdAdd from './add'
-import FossilMdTestimonial from './testimonials'
-import FossilMdSteps from './steps'
+import React from 'react';
+import FossilMdBanner from './banner';
+import FossilMdAbout from './about';
+import FossilMdPricing from './pricing';
+import FossilMdAdd from './add';
+import FossilMdTestimonial from './testimonials';
+import FossilMdSteps from './steps';
 
-
-const FossilMdLandingPage = () =>{
-    return(
-        <div className="landing-page-main">
-            <div  className="landing-header">
-            </div>
-            <div>
-            <FossilMdBanner/>
-            </div>
-            <div>
-                <FossilMdAbout/>
-            </div>
-            <div>
-                <FossilMdSteps/>
-            </div>
-            <div>
-                <FossilMdPricing/>
-            </div>
-<div>
-    <FossilMdTestimonial/>
-</div>
-            <div>
-                <FossilMdAdd/>
-            </div>
-        </div>
-    )
-}
-export default FossilMdLandingPage
+const FossilMdLandingPage = () => {
+  return (
+    <div className="landing-page-main">
+      <div className="landing-header"></div>
+      <div>
+        <FossilMdBanner />
+      </div>
+      <div>
+        <FossilMdAbout />
+      </div>
+      <div>
+        <FossilMdSteps />
+      </div>
+      <div>
+        <FossilMdPricing />
+      </div>
+      <div>
+        <FossilMdTestimonial />
+      </div>
+      <div>
+        <FossilMdAdd />
+      </div>
+    </div>
+  );
+};
+export default FossilMdLandingPage;
--- src/pages/LandingPage/steps.js
+++ src/pages/LandingPage/steps.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import icon1 from './assets/images/icon-1.png'
-import icon2 from './assets/images/icon-2.png'
-import icon3 from './assets/images/icon-3.png'
-import icon4 from './assets/images/icon-4.png'
+import icon1 from './assets/images/icon-1.png';
+import icon2 from './assets/images/icon-2.png';
+import icon3 from './assets/images/icon-3.png';
+import icon4 from './assets/images/icon-4.png';
 
 const FossilMdSteps = () => {
   return (
@@ -21,59 +21,53 @@ const FossilMdSteps = () => {
               </div>
             </Col>
           </Row>
-<div className="stepsAppointment mt6">
-          <Row>
-          <Col span={6}>
-            <div className="whiteBox">
-              <div className="iconBox">
-<img src={icon1} />
-              </div>
-              <div className="footerCaption">
-<h5>
-Appointment 
-Reminder
-</h5>
-              </div>
-            </div>
-          </Col>
-          <Col span={6}>
-          <div className="whiteBox">
-          <div className="iconBox">
-<img src={icon2} />
-              </div>
-          <div className="footerCaption">
-<h5>
-Chat
-</h5>
-              </div>
-              </div>
-          </Col>
-          <Col span={6}>
-          <div className="whiteBox">
-          <div className="iconBox">
-<img src={icon3} />
-              </div>
-          <div className="footerCaption">
-<h5>
-Booking 
-Reminder
-</h5>
-              </div>
-              </div>
-          </Col>
-          <Col span={6}>
-          <div className="whiteBox">
-          <div className="iconBox">
-<img src={icon4} />
-              </div>
-          <div className="footerCaption">
-<h5>
-Review Doctor
-</h5>
-              </div>
-              </div>
-          </Col>
-          </Row>
+          <div className="stepsAppointment mt6">
+            <Row>
+              <Col span={6}>
+                <div className="whiteBox">
+                  <div className="stepRound boxStart">1</div>
+                  <div className="iconBox">
+                    <img src={icon1} />
+                  </div>
+                  <div className="footerCaption">
+                    <h5>Appointment Reminder</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="whiteBox">
+                  <div className="stepRound boxStart">2</div>
+                  <div className="iconBox">
+                    <img src={icon2} />
+                  </div>
+                  <div className="footerCaption">
+                    <h5>Chat</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="whiteBox">
+                  <div className="stepRound boxStart">3</div>
+                  <div className="iconBox">
+                    <img src={icon3} />
+                  </div>
+                  <div className="footerCaption">
+                    <h5>Booking Reminder</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="whiteBox">
+                  <div className="stepRound boxStart">4</div>
+                  <div className="iconBox">
+                    <img src={icon4} />
+                  </div>
+                  <div className="footerCaption">
+                    <h5>Review Doctor</h5>
+                  </div>
+                </div>
+              </Col>
+            </Row>
           </div>
         </div>
       </div>
