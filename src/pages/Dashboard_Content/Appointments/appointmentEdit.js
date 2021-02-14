cf9657cef39603aec37123ea102b2dab60317c86
akshay s <7852349-akshays-rio@users.noreply.gitlab.com>
2021-02-14 22:49:13 +0530

    cc

--- src/pages/Dashboard_Content/Appointments/appointmentEdit.js
+++ src/pages/Dashboard_Content/Appointments/appointmentEdit.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field } from 'formik';
-import { ProviderCreationSchema } from '_utils/Schemas';
-import { Button, Row, Select, Col, DatePicker } from 'antd';
-import MenuItem from '@material-ui/core/MenuItem';
-import { generateForm } from '../../../_utils/formgenerator';
-import { getFormDataA } from '_utils';
-import { store } from '../../../reducers/configureStore';
-// import {Select} from 'formik-antd'
-import { TextField } from '@material-ui/core';
-import moment from 'moment'
+import { Button, Row, Select, Col,} from 'antd';
+import { DatePicker } from 'formik-antd';
+
+import moment from 'moment';
+import { TextField } from 'formik-material-ui';
 const { Option } = Select;
 const AppointmentEdit = props => {
   const [details, setDetails] = useState('');
@@ -17,13 +13,16 @@ const AppointmentEdit = props => {
   const [patientDetails, setPatientDetails] = useState('');
   const [response, setResponse] = useState([]);
 
+  const [paymentStatus, setPaymentStatus] = useState(props.view.payment_status);
+  const [status, setStatus] = useState(props.view.status);
+
   useEffect(() => {
     setDetails(props.view);
     setAppointmentType(props.view.appointment_type);
     setBranchDetails(props.view.branch);
     setResponse(props.view.response);
     setPatientDetails(props.view.patiant);
-  } , []);
+  }, []);
 
   useEffect(() => {
     console.log('jcxcxkcxcj', props.view);
@@ -31,6 +30,7 @@ const AppointmentEdit = props => {
 
   const handleFormSubmission = async values => {
     console.log('asdkjhf', values);
+    props.editAppointment(props.view.id , )
   };
 
   return (
@@ -53,111 +53,103 @@ const AppointmentEdit = props => {
 
       <Formik
         enableReinitialize={true}
-        initialValues={
-          props.values || {
-            appointment_end: '',
-            appointment_for: '',
-            appointment_start: '',
-            charge: '',
-            comment: '',
-            payment_status: '',
-            status: '',
-          }
-        }
+        initialValues={{
+          appointment_end: moment(props.view.appointment_end).format('YYYY-MM-DD hh:mm a'),
+          appointment_for: '',
+          appointment_start:moment(props.view.appointment_start).format('YYYY-MM-DD hh:mm a'),
+          charge: '',
+          comment: props.view.comment,
+          payment_status: '',
+          status: '',
+        }}
         //validationSchema={ProviderCreationSchema}
         onSubmit={handleFormSubmission}
       >
         {({ handleSubmit, touched, errors, isSubmitting }) => (
           <Form className="login__form" handleSubmit={handleSubmit}>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-
+              <Col xs={24} xl={12}>
+                <p>Payment Status</p>
+                <Select
+                  defaultValue={props.view.payment_status}
+                  style={{ width: 120 }}
+                  // name="payment_status"
+                  onChange={e => setPaymentStatus(e)}
+                  className={props.view.payment_status}
+                >
+                  <Select.Option className={'pending'} value="pending">
+                    Pending
+                  </Select.Option>
+                  <Select.Option className={'failed'} value="failed">
+                    Failed
+                  </Select.Option>
+                  <Select.Option className={'paid'} value="paid">
+                    Paid
+                  </Select.Option>
+                  <Select.Option className={'requested'} value="requested">
+                    Requested
+                  </Select.Option>
+                  <Select.Option className={'manually_paid'} value="manually_paid">
+                    Manually Paid
+                  </Select.Option>
+                </Select>
+              </Col>
 
               <Col xs={24} xl={12}>
-              <p>Payment Status</p>
-              <Select
-                // defaultValue={record.payment_status}
-                style={{ width: 120 }}
-                // onChange={e => handleChangePaymentStatus(record.id, e)}
-                //className={record.payment_status}
-              >
-                <Option className={'pending'} value="pending">
-                  Pending
-                </Option>
-                <Option className={'failed'} value="failed">
-                  Failed
-                </Option>
-                <Option className={'paid'} value="paid">
-                  Paid
-                </Option>
-                <Option className={'requested'} value="requested">
-                  Requested
-                </Option>
-                <Option className={'manually_paid'} value="manually_paid">
-                  Manually Paid
-                </Option>
-              </Select>
-
-                </Col>
-
-
-                <Col xs={24} xl={12}>
                 <p>Status</p>
-            <Select
-                //defaultValue={record.status}
-                style={{ width: 120 }}
-                //className={record.status}
-                //onChange={e => handleChangeStatus(record.id, e)}
-              >
-                <Option className={'pending'} value="pending">
-                  Pending
-                </Option>
-                <Option className={'confirmed'} value="confirmed">
-                  Confirmed
-                </Option>
-                <Option className={'cancelled'} value="cancelled">
-                  Cancelled
-                </Option>
-                <Option className={'completed'} value="completed">
-                  Completed
-                </Option>
-              </Select>
+                <Select
+                  defaultValue={props.view.status}
+                  style={{ width: 120 }}
+                  className={props.view.status}
+                  onChange={e => setStatus(e)}
+                >
+                  <Select.Option className={'pending'} value="pending">
+                    Pending
+                  </Select.Option>
+                  <Select.Option className={'confirmed'} value="confirmed">
+                    Confirmed
+                  </Select.Option>
+                  <Select.Option className={'cancelled'} value="cancelled">
+                    Cancelled
+                  </Select.Option>
+                  <Select.Option className={'completed'} value="completed">
+                    Completed
+                  </Select.Option>
+                </Select>
               </Col>
 
               <Col xs={24} xl={12}>
                 <p>Appoinment Start</p>
-              <DatePicker
-                showTime
-                //onChange={onChange}
-                //onOk={onOk}
-                name="appointment_start"
-                placeholder="Start Date"
-                showTime={{
-                  hideDisabledOptions: true,
-                  defaultValue: [moment('00:00:00', 'HH:mm:ss'), moment('11:59:59', 'HH:mm:ss')],
-                }}
-              />
+                <DatePicker
+                  showTime
+                  //onChange={onChange}
+                  //onOk={onOk}
+                  name="appointment_start"
+                  placeholder="Start Date"
+                  format="YYYY-MM-DD hh:mm a"
+                />
               </Col>
-             
 
-                <Col xs={24} xl={12}>
+              <Col xs={24} xl={12}>
                 <p>Appoinment End</p>
                 <DatePicker
-                showTime
-                //onChange={onChange}
-                //onOk={onOk}
-                name="appointment_end"
-                placeholder="End Date"
-              />
+                  showTime
+                  //onChange={onChange}
+                  //onOk={onOk}
+                  format="YYYY-MM-DD hh:mm a"
+                  name="appointment_end"
+                  placeholder="End Date"
+                />
               </Col>
               <Col xs={24} xl={12}>
                 <p>Comment</p>
-                <TextField/>
-                </Col>
-
-
-
+               <Field
+               component={TextField}
+               name='comment'
+               />
+              </Col>
             </Row>
-           
+
             <Button
               htmlType="submit"
               disabled={isSubmitting}
