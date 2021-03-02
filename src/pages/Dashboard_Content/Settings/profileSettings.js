39f7ed694358e6b7c8ff390863bed855500af0f3
akshays-rio <akshay.s@riolabz.com>
2021-03-02 12:18:21 +0530

    forms table responsive and profile settigns added

--- src/pages/Dashboard_Content/Settings/profileSettings.js
+++ src/pages/Dashboard_Content/Settings/profileSettings.js
@@ -1,11 +1,153 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react';
+import { generateForm } from '../../../_utils/formgenerator';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { getFormDataA } from '_utils';
+import { Thumb } from './thumb';
+import { useDropzone } from 'react-dropzone';
+import { Row, Button } from 'antd';
 
-const ProfileSettings =() =>{
-    return(
-        <div>
-            Profile settings
-        </div>
-    )
-}
+const ProfileSettings = props => {
+  const formField = [
+    {
+      label: 'Hospital Name',
+      name: 'fullName',
+      type: 'text',
+    },
+    {
+      label: 'Phone no.',
+      name: 'phone',
+      type: 'text',
+    },
+    {
+        label: 'New Password',
+        name: 'password',
+        type: 'password',
+      },
+      {
+        label: 'Confirm New Password',
+        name: 'c_password',
+        type: 'password',
+      },
+  ];
 
-export default ProfileSettings
+  const [files, setFiles] = useState([]);
+  const innerForm = useRef();
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setFiles(
+        acceptedFiles.map(file =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          }),
+        ),
+      );
+
+      innerForm.current.setFieldValue('logo', acceptedFiles);
+    },
+  });
+
+
+  useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach(file => URL.revokeObjectURL(file.preview));
+    },
+    [files],
+  );
+  const onImageDelete = name => {
+    setFiles(prev => prev.filter(result => result.name != name));
+    innerForm.current.setFieldValue(
+      'logo',
+      innerForm.current.values.logo.filter(result => result.name != name),
+    );
+  };
+  const handleFormSubmission = () => {};
+
+  return (
+    <div>
+      <Formik
+        enableReinitialize={true}
+        initialValues={{
+          fullName: '',
+          phone: '',
+          password: '',
+          c_password: '',
+          logo: '',
+        }}
+        //  validationSchema={props.editId ? hospitaEditlUser : hospitalUser}
+        onSubmit={handleFormSubmission}
+        innerRef={innerForm}
+      >
+        {({ handleSubmit, touched, errors, isSubmitting, values }) => (
+          <Form className="login__form" handleSubmit={handleSubmit}>
+            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                {generateForm(formField)}
+            </Row>
+
+            <p>
+              <div
+                style={{
+                  // padding: 10,
+                  // borderRadius: 5,
+                  width: '100%',
+                }}
+                {...getRootProps({ className: 'dropzone' })}
+              >
+                <div className="title">
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      flexWrap: 'wrap',
+                    }}
+                  >
+                    {Array.isArray(values.logo) ? (
+                      values.logo.map((result, i) => (
+                        <Thumb onImageDelete={onImageDelete} file={result}></Thumb>
+                      ))
+                    ) : (
+                      <img
+                        style={{
+                          width: 90,
+                          height: 90,
+                          borderRadius: 100,
+                          marginRight: 10,
+                          objectFit: 'cover',
+                        }}
+                        src={
+                          values.logo ||
+                          'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'
+                        }
+                      />
+                    )}
+                  </div>
+                  <input {...getInputProps()} multiple={false} />
+                  <div className="upload-btn-wrapper">
+                    <button type="button" className="view-button button-square font-size-md px-5">
+                      Browse Image
+                    </button>
+                  </div>
+                  {/* <span>Upload Patient Image</span> */}
+                </div>
+              </div>
+
+              {errors.logo && <div className="errormsg">{errors.logo}</div>}
+            </p>
+
+            <Button
+              htmlType="submit"
+              disabled={isSubmitting}
+              className="view-button button-square mt-5"
+            >
+              {' Save Changes' }
+            </Button>
+          </Form>
+        )}
+      </Formik>{' '}
+    </div>
+  );
+};
+
+export default ProfileSettings;
